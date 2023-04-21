a82c37f259d8dd32377ac5c5a50ebb1e71ed406a
zpdl95 <won8070@naver.com>
2023-04-21 22:57:12 +0900

    remove baseUrl & update package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./Components/Header";
-import Home from "./Routes/Home";
-import Search from "./Routes/Search";
-import TV from "./Routes/TV";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './Routes/Home';
+import Search from './Routes/Search';
+import TV from './Routes/TV';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Header />
       <Switch>
         <Route
-          path={["/tvs", "/tvs/:slideId/:tvId"]}
+          path={['/tvs', '/tvs/:slideId/:tvId']}
           component={() => <TV />}
         />
-        <Route path={"/search"} component={() => <Search />} />
+        <Route path={'/search'} component={() => <Search />} />
         {/* path를 array로 여러개 사용 가능 */}
         <Route
-          path={["/", "/movies/:slideId/:movieId"]}
+          path={['/', '/movies/:slideId/:movieId']}
           component={() => <Home />}
         />
       </Switch>
